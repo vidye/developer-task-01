46184ca9b485a3b0e9fd9006b55b2a6875c115ce
Evidence <vidyehlabiswana@gmail.com>
2021-04-20 09:25:42 +0200

    Fixed method subscriberRequestDao.persist(subscriberRequest) to subscriberRequestDao.save(subscriberRequest)

--- electronic-payments-business/src/main/java/com/econetwireless/epay/business/services/impl/EnquiriesServiceImpl.java
+++ electronic-payments-business/src/main/java/com/econetwireless/epay/business/services/impl/EnquiriesServiceImpl.java
@@ -33,7 +33,7 @@ public class EnquiriesServiceImpl implements EnquiriesService {
         LOGGER.info("Enquire airtime balance :: Partner Code : {}, Msisdn : {}", partnerCode, msisdn);
         final AirtimeBalanceResponse airtimeBalanceResponse = new AirtimeBalanceResponse();
         final SubscriberRequest subscriberRequest = populate(partnerCode, msisdn);
-        final SubscriberRequest createdSubscriberRequest = subscriberRequestDao.persist(subscriberRequest);
+        final SubscriberRequest createdSubscriberRequest = subscriberRequestDao.save(subscriberRequest);
         final INBalanceResponse inBalanceResponse = chargingPlatform.enquireBalance(partnerCode, msisdn);
         changeSubscriberStateOnBalanceEnquiry(createdSubscriberRequest, inBalanceResponse);
         subscriberRequestDao.update(createdSubscriberRequest);
